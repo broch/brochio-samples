82728b1d8619629f7287a146d8cd735a446598d8
Luke Taylor <tekul.hs@gmail.com>
2015-01-06 18:24:41 +0000

    Add request access functions

--- ByoWai.hs
+++ ByoWai.hs
@@ -2,6 +2,7 @@
 
 module YaWai where
 
+import Control.Monad.Error (throwError)
 import Control.Monad.Reader
 import Control.Monad.State
 import Control.Monad.Trans.Either (EitherT, runEitherT)
@@ -10,9 +11,10 @@ import qualified Data.Map.Strict as M
 import Data.Text (Text)
 import qualified Data.Text.Encoding as TE
 import Data.ByteString (ByteString)
+import qualified Data.ByteString as B
 import qualified Data.ByteString.Lazy as BL
 import Network.HTTP.Types (ResponseHeaders, Status, internalServerError500, status200, status302, hLocation)
-import Network.Wai (Request, Response, queryString, responseLBS)
+import Network.Wai (Request, Response, queryString, responseLBS, strictRequestBody)
 import Network.Wai.Parse (parseRequestBody, lbsBackEnd)
 
 
@@ -59,4 +61,19 @@ runHandler req h  = do
 
 toMap :: [(ByteString, ByteString)] -> Params
 toMap = M.unionsWith (++) . map (\(x, y) -> M.singleton (TE.decodeUtf8 x) [TE.decodeUtf8 y])
+
+-- Read request data
+postParam :: Text -> Handler Text
+postParam name = asks postParams >>= lookupParam name
+
+queryParam :: Text -> Handler Text
+queryParam name = asks queryParams >>= lookupParam name
+
+lookupParam :: Text -> Params -> Handler Text
+lookupParam name params = case M.lookup name params of
+    Just [v] -> return v
+    _        -> throwError $ HandlerError $ B.concat ["Missing or duplicate parameter", TE.encodeUtf8 name]
+
+body :: Handler BL.ByteString
+body = asks waiReq >>= liftIO . strictRequestBody
 
