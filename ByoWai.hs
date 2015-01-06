fd9ed69728522d2c2c9ecc151d35549caa60555f
Luke Taylor <tekul.hs@gmail.com>
2015-01-06 18:13:29 +0000

    Add the runHandler function

--- ByoWai.hs
+++ ByoWai.hs
@@ -4,16 +4,19 @@ module YaWai where
 
 import Control.Monad.Reader
 import Control.Monad.State
-import Control.Monad.Trans.Either (EitherT)
-import Data.Map.Strict (Map)
+import Control.Monad.Trans.Either (EitherT, runEitherT)
+import Data.Maybe (fromMaybe)
+import qualified Data.Map.Strict as M
 import Data.Text (Text)
+import qualified Data.Text.Encoding as TE
 import Data.ByteString (ByteString)
 import qualified Data.ByteString.Lazy as BL
-import Network.HTTP.Types (ResponseHeaders, Status)
-import Network.Wai (Request)
+import Network.HTTP.Types (ResponseHeaders, Status, internalServerError500, status200, status302, hLocation)
+import Network.Wai (Request, Response, queryString, responseLBS)
+import Network.Wai.Parse (parseRequestBody, lbsBackEnd)
 
 
-type Params = Map Text [Text]
+type Params = M.Map Text [Text]
 
 data RequestData = RequestData
     { waiReq :: Request
@@ -35,4 +38,25 @@ data HandlerResult = Redirect ByteString     -- Redirect to a URL
 type Handler a = EitherT HandlerResult (ReaderT RequestData (StateT ResponseState IO)) a
 
 type Router = [Text] -> Handler ()
+
+runHandler :: Request -> Handler () -> IO Response
+runHandler req h  = do
+    (pParams, _) <- parseRequestBody lbsBackEnd req
+    let initRes = ResponseState status200 [] ""
+        rd = RequestData
+              { waiReq      = req
+              , queryParams = toMap $ fmap (\(n, v) -> (n, fromMaybe "" v)) $ queryString req
+              , postParams  = toMap pParams
+              }
+
+    (result, res) <- runStateT (runReaderT (runEitherT h) rd) initRes
+    let hdrs = resHeaders res
+    return $ case result of
+        Left ResponseComplete   -> responseLBS (resStatus res) hdrs (content res)
+        Left (Redirect url)     -> responseLBS status302 ((hLocation, url) : hdrs) ""
+        Left (HandlerError msg) -> responseLBS internalServerError500 hdrs (BL.fromStrict msg)
+        Right _ -> error "Not handled"
+
+toMap :: [(ByteString, ByteString)] -> Params
+toMap = M.unionsWith (++) . map (\(x, y) -> M.singleton (TE.decodeUtf8 x) [TE.decodeUtf8 y])
 
