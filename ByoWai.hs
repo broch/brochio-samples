8fcb01665d47eb5f36195fcea8d81dfe6f4d0c44
Luke Taylor <tekul.hs@gmail.com>
2015-01-06 18:06:53 +0000

    Add EitherT HandlerResult to Handler type

--- ByoWai.hs
+++ ByoWai.hs
@@ -4,8 +4,10 @@ module YaWai where
 
 import Control.Monad.Reader
 import Control.Monad.State
+import Control.Monad.Trans.Either (EitherT)
 import Data.Map.Strict (Map)
 import Data.Text (Text)
+import Data.ByteString (ByteString)
 import qualified Data.ByteString.Lazy as BL
 import Network.HTTP.Types (ResponseHeaders, Status)
 import Network.Wai (Request)
@@ -25,6 +27,12 @@ data ResponseState = ResponseState
     , content :: BL.ByteString
     }
 
-type Handler a = ReaderT RequestData (StateT ResponseState IO) a
+data HandlerResult = Redirect ByteString     -- Redirect to a URL
+                   | ResponseComplete        -- Send the response
+                   | HandlerError ByteString -- Send an internal error response
+                     deriving (Show, Eq)
+
+type Handler a = EitherT HandlerResult (ReaderT RequestData (StateT ResponseState IO)) a
+
 type Router = [Text] -> Handler ()
 
